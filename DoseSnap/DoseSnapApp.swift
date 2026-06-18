6ab4d33eca8b3db445eb0785a2e9b6899007a73e
Dwennk <dwenn@live.fr>
2026-06-18 11:46:21 +0200

    Initial Commit

---
+++ DoseSnap/DoseSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoseSnapApp.swift
+//  DoseSnap
+//
+//  Created by Dwenn on 18.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct DoseSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
